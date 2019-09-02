6c898379f8f5ba6d53ba299d3292f1af26b96ab4
MenesiZoltan <colos.menesi@gmail.com>
2019-09-02 15:47:35 +0200

    Feature 3 commit 1

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Feature3");
             Console.ReadLine();
         }
     }

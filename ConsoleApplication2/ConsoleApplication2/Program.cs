18f422e8caedd251504f3dabce6ffcf0d9ec054c
MenesiZoltan <colos.menesi@gmail.com>
2019-09-03 10:55:15 +0200

    Branch2

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -13,10 +13,6 @@ namespace ConsoleApplication2
             Console.WriteLine("Hello World");
             Console.WriteLine("2.2");
             Console.WriteLine("Feature3.2");
-            Console.WriteLine("Sending Pull Request");
-            Console.WriteLine("Sending Pull Request2");
-            Console.WriteLine("Sending Pull Request3");
-            Console.WriteLine("Sending Pull Request4");
             Console.WriteLine("Sending Pull Request5");
             Console.ReadLine();
         }

f6e98e7f297bba9fd46e865001c6152b7739e427
MenesiZoltan <colos.menesi@gmail.com>
2019-09-05 14:32:13 +0200

    Added new stuff

---
+++ ConsoleApplication2/ConsoleApplication2/Speaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public static class Speaker
+    {
+        public static void SpeakSomething(string something)
+        {
+            Console.WriteLine(something);
+        }
+    }
+}
